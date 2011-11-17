21e9cec087bb31be567d46cebaf42cc5afebe8f6
jugyo <jugyo.org@gmail.com>
2011-11-17 21:09:42 +0900

    ignore_pattern を指定できるようにした
    rails の form_helper が helpers.label.xxx というような key で翻訳データを探そうとするのでその対策
    本来は rails の初期化処理のタイミングで ignore_pattern をセットするようにすべきだと思う

--- lib/i81n.rb
+++ lib/i81n.rb
@@ -1,6 +1,17 @@
 require 'i18n'
 class I18n::Backend::I81n < I18n::Backend::Simple
+  attr_reader :ignore_pattern
+
+  def initialize(ignore_pattern = nil)
+    @ignore_pattern = ignore_pattern
+    super
+  end
+
   def lookup(locale, key, scope = [], options = {})
-    super || key.to_s
+    if ignore_pattern && ignore_pattern =~ key.to_s
+      super
+    else
+      super || key.to_s
+    end
   end
 end
